1c1a265d00a7f7b1886fdc23f4320b3841aac244
Justas <zhongli3137@gmail.com>
2024-11-09 13:19:02 +0200

    Initial frontend setup

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<div class="p-5">
+	<h1 class="font-medium">Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit" target="_blank" class="text-blue-500"
+			>svelte.dev/docs/kit</a
+		> to read the documentation
+	</p>
+
+	<p class="mt-2">^</p>
+	<p>^</p>
+	<p>^</p>
+	<p>^</p>
+	<p>^</p>
+	<p class="font-medium text-red-500">The power of Tailwind CSS!</p>
+	<p>
+		Also visit <a href="https://tailwindcss.com/docs/" target="_blank" class="text-blue-500"
+			>tailwindcss.com/docs/</a
+		> to read the documentation
+	</p>
+</div>
