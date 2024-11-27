b6fc9feb8d7b8da2434f7cfc68e535e4ed684b88
Justas <zhongli3137@gmail.com>
2024-11-27 18:47:58 +0200

    Add Footer.svelte, CR title. Update .gitignore

---
+++ frontend/src/components/Footer.svelte
@@ -0,0 +1,6 @@
+<footer class="flex h-12 border-t border-gray-400 bg-white">
+	<img alt="Kavos Skrudykla logo" src="logo.png" class="ml-5 h-7 w-7 self-center rounded-full" />
+	<p class="ml-5 self-center text-xs font-medium text-gray-400">
+		&copy; 2024 UAB "Kavos Skrudykla"
+	</p>
+</footer>
